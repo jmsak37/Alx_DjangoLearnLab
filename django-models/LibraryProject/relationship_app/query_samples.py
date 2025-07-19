871b1cc9f0d6ef3ae3a198f14c1035f9775c7d5d
julius samuel <jmsak37@gmail.com>
2025-07-19 17:25:05 +0300

    Import django-models as normal directory (remove submodule)

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,26 +1,29 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+# Bootstrap Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
-from relationship_app.models import Author, Book, Library
+from relationship_app.models import Author, Library, Librarian
 
-def run_queries():
-    # 1. All books by a specific author
-    author = Author.objects.first()
-    print(f"Books by {author.name}:")
-    for b in author.books.all():
-        print(" -", b.title)
+def run_queries(author_name, library_name):
+    # 1. Query all books by a specific author.
+    author = Author.objects.get(name=author_name)
+    print(f"Books by {author_name}:")
+    for book in author.books.all():
+        print(" -", book.title)
 
-    # 2. List all books in a library
-    library = Library.objects.first()
-    print(f"\nBooks in {library.name}:")
-    for b in library.books.all():
-        print(" -", b.title)
+    # 2. List all books in a library.
+    library = Library.objects.get(name=library_name)
+    print(f"\nBooks in {library_name}:")
+    for book in library.books.all():
+        print(" -", book.title)
 
-    # 3. Retrieve the librarian for a library
-    print(f"\nLibrarian at {library.name}: {library.librarian.name}")
+    # 3. Retrieve the librarian for a library.
+    librarian = Librarian.objects.get(library=library)
+    print(f"\nLibrarian at {library_name}: {librarian.name}")
 
 if __name__ == '__main__':
-    run_queries()
+    # TODO: replace these with actual names in your DB
+    run_queries('Some Author', 'Central Library')
