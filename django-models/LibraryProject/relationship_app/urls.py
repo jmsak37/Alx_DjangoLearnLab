5661f4a6cd10b3b7c158ddb3a735d1e1de83273a
julius samuel <jmsak37@gmail.com>
2025-07-19 18:23:32 +0300

    Task 2: Consolidate auth imports and add login_view/logout_view to views.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,22 +1,21 @@
-from django.contrib.auth import views as auth_views
-from .views import RegisterView
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # registration
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
 
     # login
-    path('login/', auth_views.LoginView.as_view(
+    path('login/', LoginView.as_view(
         template_name='relationship_app/login.html'
     ), name='login'),
 
     # logout
-    path('logout/', auth_views.LogoutView.as_view(
+    path('logout/', LogoutView.as_view(
         template_name='relationship_app/logout.html'
     ), name='logout'),
 ]
