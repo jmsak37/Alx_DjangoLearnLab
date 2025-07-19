e63dfdae244512b1fd3b95b3ad92cb261443aff9
julius samuel <jmsak37@gmail.com>
2025-07-19 17:48:10 +0300

    Clean up views.py imports: use single relative import from .models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
-from .models import Book, Library
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from relationship_app.models import Book, Library
 
 # 1. Function-based view: list all books
 def list_books(request):
-    books = Book.objects.all()  # explicit .all()
-    return render(request, 'relationship_app/list_books.html', {
-        'books': books
-    })
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # 2. Class-based view: show one library’s details
 class LibraryDetailView(DetailView):
