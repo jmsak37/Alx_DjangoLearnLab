0d0579bdb9ab363816d154037715a9f3119ecf25
julius samuel <jmsak37@gmail.com>
2025-07-19 17:27:21 +0300

    Import django-models as normal directory (remove submodule)

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,19 +5,21 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
-from relationship_app.models import Author, Library, Librarian
+from relationship_app.models import Author, Book, Library, Librarian
 
 def run_queries(author_name, library_name):
     # 1. Query all books by a specific author.
     author = Author.objects.get(name=author_name)
+    books_by_author = Book.objects.filter(author=author)
     print(f"Books by {author_name}:")
-    for book in author.books.all():
+    for book in books_by_author:
         print(" -", book.title)
 
     # 2. List all books in a library.
     library = Library.objects.get(name=library_name)
+    books_in_library = library.books.all()
     print(f"\nBooks in {library_name}:")
-    for book in library.books.all():
+    for book in books_in_library:
         print(" -", book.title)
 
     # 3. Retrieve the librarian for a library.
@@ -25,5 +27,5 @@ def run_queries(author_name, library_name):
     print(f"\nLibrarian at {library_name}: {librarian.name}")
 
 if __name__ == '__main__':
-    # TODO: replace these with actual names in your DB
+    # Replace with actual names from your DB
     run_queries('Some Author', 'Central Library')
