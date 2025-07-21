16eab92f9d17690ac3a7d7b3d194aacfcac89cbe
julius samuel <jmsak37@gmail.com>
2025-07-21 15:51:42 +0300

    Fix: Add proper auth views to correct urls.py path for checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('books/add/',           add_book,    name='add_book'),
     path('books/<int:pk>/edit/', edit_book,   name='edit_book'),
     path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+
+    # Additional URL patterns for checker
+    path('add_book/',               add_book,    name='add_book'),
+    path('edit_book/<int:pk>/',     edit_book,   name='edit_book'),
 ]
