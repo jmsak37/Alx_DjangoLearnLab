d40268b752b804aa321b06965057b1a0a6dfd432
julius samuel <jmsak37@gmail.com>
2025-07-21 06:09:51 +0300

    Fix URLs: use views.register and include LoginView/LogoutView.with template_name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # registration
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 
     # login
     path('login/', LoginView.as_view(
