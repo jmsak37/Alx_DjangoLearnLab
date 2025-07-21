b1c126d161e3d1a9f84e43077df885c3679c62bc
julius samuel <jmsak37@gmail.com>
2025-07-21 09:44:09 +0300

    Fix: Add proper auth views to correct urls.py path for checker

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    # Book & Library views
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication
+    path('register/', views.register, name='register'),
+    path('login/',    LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/',   LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+]
+
