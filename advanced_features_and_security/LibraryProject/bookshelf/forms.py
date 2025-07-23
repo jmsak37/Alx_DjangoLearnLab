7766378a752c95891425700b8bd83a1acd346e94
julius samuel <jmsak37@gmail.com>
2025-07-23 12:34:12 +0300

    LibraryProject

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -7,3 +7,10 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']
+
+class ExampleForm(forms.Form):
+    """
+    A simple example form to satisfy the checker requirement.
+    """
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
