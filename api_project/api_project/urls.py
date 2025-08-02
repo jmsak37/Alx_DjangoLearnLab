642a351bb55344a0f8a662d8eb5f40917e41955b
julius samuel <jmsak37@gmail.com>
2025-08-02 12:13:09 +0300

    api_project

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,3 +1,7 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
 """api_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,10 +17,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api_app.urls')),
+    path('api/', include('api.urls')),
 ]
