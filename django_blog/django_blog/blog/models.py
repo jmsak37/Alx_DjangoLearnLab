ae1384425c10138ed8ba82f14067e72896ad7be5
julius samuel <jmsak37@gmail.com>
2025-08-17 12:08:18 +0300

    Initial django_blog project: blog app, Post model, views, templates, static

---
+++ django_blog/django_blog/blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Post(models.Model):
+    """
+    Blog Post model for django_blog.
+    Fields:
+    - title: short title
+    - content: post body
+    - published_date: automatically set on creation
+    - author: FK to the User model (allow many posts per author)
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
