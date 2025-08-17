5a30c84b9a979fc05e675f57c3e89a675ecbb552
julius samuel <jmsak37@gmail.com>
2025-08-17 12:23:27 +0300

    Initial django_blog project: blog app, Post model, views, templates, static

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,9 +77,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'postgres_example': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
