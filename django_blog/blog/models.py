f999d2a5589cf81711a08bee137d587e75ab1323
julius samuel <jmsak37@gmail.com>
2025-08-17 14:05:16 +0300

    django_blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.conf import settings
+from django.utils import timezone
+
 from django.urls import reverse
 User = get_user_model()
 
@@ -24,3 +27,22 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post-detail', kwargs={'pk': self.pk})
+
+
+# Comment model: attached to a Post and a User. Stores timestamps for created/updated.
+class Comment(models.Model):
+    # link to the Post model (assumes Post is defined in this module)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    # link to the configured user model (works with default or custom AUTH_USER_MODEL)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
