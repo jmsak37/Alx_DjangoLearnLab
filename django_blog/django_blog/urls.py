bfbeb743c2d2b7f121aff7118a18bbce83ceee76
julius samuel <jmsak37@gmail.com>
2025-08-17 15:24:49 +0300

    django_blog

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,30 +1,24 @@
-"""
-URL configuration for django_blog project.
+# blog/urls.py
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path include
-from django.views.generic import TemplateView
+app_name = "blog"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('blog.urls')),
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
-    path('', TemplateView.as_view(template_name='blog/home.html'), name='home'),
-    path('', include('blog.urls', namespace='blog')),
-    path('accounts/', include('blog.urls')),
-    path('search/', views.post_search, name='post_search'),
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
+    # Comments
+    path('post/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+
+    # Search
+    path('search/', views.post_search, name='post-search'),
+
+    # Tags
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
 ]
