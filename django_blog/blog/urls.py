cf75ca6b6f74809fe0b03574d0c91fe55bc8f49b
julius samuel <jmsak37@gmail.com>
2025-08-17 12:52:25 +0300

    django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
+path('register/', views.register, name='register'),
+
+    # Login / Logout using built-in views but with our templates
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+
+    # Profile (requires login)
+    path('profile/', views.profile, name='profile'),
 ]
